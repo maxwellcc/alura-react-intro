9b49f2b01dd65e0ec07d08caf84707566171a119
Maxwell Anderson Ielpo do Amaral <max@maxwellanderson.com.br>
2022-02-03 11:00:47 -0300

    Aula 05.02

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,32 +3,44 @@ import "./estilo.css";
 
 class FormularioCadastro extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.titulo = "";
+        this.texto = "";
     }
-    
-    handleMudancaTitulo(evento){
-       this.titulo = evento.target.value;
-       console.log(this.titulo)
+
+    _handleMudancaTitulo(evento) {
+        this.titulo = evento.target.value;
+    }
+
+    _handleMudancaTexto(evento) {
+        this.texto = evento.target.value;
+    }
+
+    _criarNota(evento){
+        evento.preventDefault(); // evita que a página seja recarregada
+        evento.stopPropagation(); // evita que o evento seja propagado para os outros componentes
+
+        this.props.criarNota(this.titulo, this.texto);
     }
 
     render() {
         return (
-            <form 
+            <form
                 className="form-cadastro"
-                
+                onSubmit={this._criarNota.bind(this)}
             >
                 <input
                     type="text"
                     placeholder="Título"
                     className="form-cadastro_input"
-                    onChange={this.handleMudancaTitulo.bind(this)}
+                    onChange={this._handleMudancaTitulo.bind(this)}
                 />
                 <textarea
                     rows={15}
                     placeholder="Escreva sua nota..."
                     className="form-cadastro_input"
+                    onChange={this._handleMudancaTexto.bind(this)}
                 />
                 <button className="form-cadastro_input form-cadastro_submit">
                     Criar Nota
