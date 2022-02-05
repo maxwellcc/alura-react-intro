525f354c4a3ec682d88faf1bff1465430779ad22
Maxwell Andeson <max@max.maxwellanderson.local>
2022-02-04 21:30:49 -0300

    Base Curso 01, Aula 01.04

--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -9,7 +9,12 @@ class ListaDeNotas extends Component {
         {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota titulo={nota.titulo} texto={nota.texto}/>
+              <CardNota
+                  deletarNota={this.props.deletarNota}
+                  titulo={nota.titulo}
+                  texto={nota.texto}
+                  indice={index}
+              />
             </li>
           );
         })}
